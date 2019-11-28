b83c7f9b1013128c8f09c89ac59b2af569e6d376
Gitx86 <32510277+Gitx86@users.noreply.github.com>
2019-11-29 00:52:32 +0800

    Add files via upload

---
+++ emission.py
@@ -0,0 +1,17 @@
+def emission(node,word,x_dict):
+    global emission_dict
+    pair = word,node
+    detector = 0 # this is used to find if word exist in the dictionary
+    if pair not in emission_dict.keys(): #if the combination cannot be found in the dictionary
+                                         #Either the word exists, or word is new. 
+        if word in x_dict:
+            score=0   #this means that this node is not the correct node.
+        else:
+            replaced_text = ("#UNK#", node)
+            if replaced_text in emission_dict.keys():
+                score = emission_dict[replaced_text] #if label have #unk#
+            else:
+                score = 0   #if label does not have #unk#, then set to 0.
+    else:
+        score = emission_dict[pair]
+    return score
