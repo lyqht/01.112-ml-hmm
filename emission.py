73e7d7be60f7ffe5556f7a4f0aa7acb643d429ad
Gitx86 <32510277+Gitx86@users.noreply.github.com>
2019-11-29 11:08:51 +0800

    Update emission.py

--- emission.py
+++ emission.py
@@ -1,3 +1,4 @@
+############### NEW   ############
 def emission(node,word,x_dict):
     global emission_dict
     pair = word,node
@@ -8,6 +9,30 @@ def emission(node,word,x_dict):
             score=0   #this means that this node is not the correct node.
         else:
             replaced_text = ("#UNK#", node)
+            if replaced_text in emission_dict.keys():
+                score = emission_dict[replaced_text] #if label have #unk#
+            else:
+                score = 0   #if label does not have #unk#, then set to 0.
+    else:
+        score = emission_dict[pair]
+    return score
+
+############### OLD ############
+def emission(node,word):
+    global emission_dict
+    global nodes
+    pair = word+" "+node
+    detector = 0 # this is used to find if word exist in the dictionary
+    if pair not in emission_dict.keys(): #if the combination cannot be found in the dictionary
+                                         #Either the word exists, or word is new. 
+        for o in nodes:
+            missing_pair = word+" "+o
+            if missing_pair in emission_dict.keys(): #
+                detector = 1 # to detect if word exist in dictionary.
+        if detector == 1:
+            score=0   #this means that this node is not the correct node.
+        else:
+            replaced_text = "#UNK# "+ node
             if replaced_text in emission_dict.keys():
                 score = emission_dict[replaced_text] #if label have #unk#
             else:
